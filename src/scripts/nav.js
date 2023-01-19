98e1c20abae1c2867e07fb9b5acc3d699842a2d7
Zatfley <alfi.dim1612@gmail.com>
2023-01-19 15:11:54 +0700

    first commit

---
+++ src/scripts/nav.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', () => {
+        const drawerElement = document.getElementById('drawer')
+        if (window.scrollY != 0) {
+            drawerElement.style.position = 'fixed';
+        } else {
+            drawerElement.style.position = 'relative'
+        }
+    });
+
+    const hamburger = document.getElementById('hamburger');
+    const main = document.querySelector('main');
+    const navList = document.querySelector('.nav__list');
+
+    hamburger.addEventListener('click', event => {
+        navList.classList.toggle('open');
+        event.stopPropagation();
+    })
+
+    main.addEventListener('click', event => {
+        navList.classList.remove('open');
+        event.stopPropagation();
+    })
+})
