5fa1960b9821dc7dc1912ef4abb0d7d920dd8c50
Zatfley <alfi.dim1612@gmail.com>
2023-02-22 19:45:26 +0700

    fix bundler analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: {
@@ -62,5 +63,7 @@ module.exports = {
         },
       ],
     }),
+
+    new BundleAnalyzerPlugin(),
   ],
 };
